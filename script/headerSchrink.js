39e0cb013c1198f213fed0ebad00bcb224c592a0
Kilian DUCLOS <97612798+BonelessGod@users.noreply.github.com>
2022-03-30 10:15:27 +0200

    update

--- script/headerSchrink.js
+++ script/headerSchrink.js
@@ -1,26 +1,17 @@
-window.onscroll = function() {setTimeout(headerSchrink, 500)};
+window.onscroll = function() {headerSchrink()};
 
-shrinkedValue = "5rem";
-unshrinkedValue = "10rem";
+const headerToSchrink = document.getElementById('header');
 
-const buttonMenu = document.querySelectorAll('.buttonMenu');
 
 function headerSchrink() {
-    if (window.scrollY > 5) {
-        document.getElementById("header").style.height = "8rem";
-        document.getElementById("header").style.flexDirection = "row";
-        document.getElementById("headerImage").style.width = shrinkedValue;
-        document.getElementById("headerImage").style.height = shrinkedValue;
-        buttonMenu.forEach(btn => {
-            btn.style.width = '6rem';
-        });
+    if (window.scrollY > 100) {
+        
+        headerToSchrink.classList.add("headerShrinked")
+        
     } else {
-        document.getElementById("header").style.height = "15rem";
-        document.getElementById("header").style.flexDirection = "column";
-        document.getElementById("headerImage").style.width = unshrinkedValue;
-        document.getElementById("headerImage").style.height = unshrinkedValue;
-        buttonMenu.forEach(btn => {
-            btn.style.width = '10rem';
-        });
+
+        headerToSchrink.classList.remove("headerShrinked")
+       
     }
+    console.log(scrollY)
 }
